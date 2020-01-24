c525ff4ae72e5c0996e53cf048ed5a7bcc819cf6
LucasMCruz <lucasmatheus644@gmail.com>
2020-01-24 14:57:21 -0300

    Add layout padrao

--- src/main/java/br/com/gft/controller/PortController.java
+++ src/main/java/br/com/gft/controller/PortController.java
@@ -17,9 +17,25 @@ public class PortController {
 	}
 	
 	@RequestMapping("/about")
-	public String about() {
+	public String Sobre() {
 		return "About";
 	}
+	@RequestMapping("/services")
+	public String Skill() {
+		return "Services";
+	}
 	
+	@RequestMapping("/work")
+	public String Work() {
+		return "Work";
+	}
+	@RequestMapping("/blog")
+	public String LifeStyle() {
+		return "Blog";
+	}
+	@RequestMapping("/contato")
+	public String Contact() {
+		return "Contato";
+	}
 	
 }
